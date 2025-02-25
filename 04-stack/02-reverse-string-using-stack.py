c9da7e8d4ccaa702841bc5e29f0c8f02dda84304
Kudratkhuja Musaev <kudratkhujamusaev@gmail.com>
2025-02-25 17:41:41 +0000

    Implement reverse string solution using stack with test cases

---
+++ 04-stack/02-reverse-string-using-stack.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reverseString(self, s):
+        stack = []
+
+        for char in s:
+            stack.append(char)
+        
+        reverse_s = ''
+
+        while stack:
+            reverse_s += stack.pop()
+
+        return reverse_s
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    
+    # Test cases
+    test_cases = {
+        "hello" : "olleh",
+        "world" : "dlrow",
+        "python" : "nohtyp",
+        "" : ""
+    }
+    
+    for s, expected in test_cases.items():
+        result = solution.reverseString(s)
+        print(f"Input: {s}, Expected: {expected}, Result: {result}, Passed: {expected == result}")
+    
